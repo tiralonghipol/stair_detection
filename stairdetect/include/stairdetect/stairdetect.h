27b30fac08f232a643f5878681cc0a8cb6752d1d
rrtroutslater <rer87@cornell.edu>
2020-01-06 15:45:53 -0800

    birds eye image timing trigger

--- stairdetect/include/stairdetect/stairdetect.h
+++ stairdetect/include/stairdetect/stairdetect.h
@@ -28,7 +28,7 @@
 #include <opencv2/imgproc.hpp>
 
 #include <cv_bridge/cv_bridge.h>
-
+#include <image_transport/image_transport.h>
 
 using namespace std;
 using namespace cv;
@@ -39,6 +39,12 @@ struct stairDetectorParams
 	double canny_low_th = 40;
 	double canny_ratio = 10;
 	int canny_kernel_size = 1;
+
+    // pcl to img params
+    // THESE SHOULD PROBABLY COME FROM LIDAR_STITCH_PARAMS
+    double img_xy_dim = 25;
+    double img_resolution = 0.02;
+
 };
 
 class stairDetector{
@@ -50,6 +56,7 @@ class stairDetector{
     // void callback_stitched_pcl(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr & msg);
     void callback_stitched_pcl(const pcl::PointCloud<pcl::PointXYZ>::Ptr & msg);
     void callback_pose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr & msg);
+    void callback_timer_trigger(const ros::TimerEvent& event);
 
     // other functions
     void trim_stitched_pcl(pcl::PCLPointCloud2 & trimmed_cloud);
@@ -57,11 +64,11 @@ class stairDetector{
 
     // image operations
     // void filter_img(const cv::Mat &bird_view_img);
+    void pcl_to_bird_view_img(const pcl::PointCloud<pcl::PointXYZ>::Ptr & cloud);
     void filter_img();
     void cannyEdgeDetection(const cv::Mat &input_image, cv::Mat &edge);
 
   	// void houghLine(const cv::Mat &edge_image, Lines &lines);
-
 
     private:
     // subscribers
@@ -70,23 +77,29 @@ class stairDetector{
 
     // publishers
     ros::Publisher _pub_trimmed_pcl;
+    image_transport::Publisher _pub_bird_view_img;
+
+    // timers
+    ros::Timer _timer_stair_detect;
 
     // topic names
     std::string _topic_stitched_pcl;
     std::string _topic_pose;
-    std::string _topic_trimmed_pcl;  
+    std::string _topic_trimmed_pcl;
+    std::string _topic_bird_eye_img;
 
     // pointcloud trimming params
     int _xy_lim;
     int _z_lim;
+    double _stair_detect_time;
 
-    // test mode flag
-    // bool _test_mode;
+    // reference to most recently-received cloud
+    pcl::PointCloud<pcl::PointXYZ>::Ptr _recent_cloud;
 
     // parameter container 
     stairDetectorParams _param;
 
-    // 
+    // pose queue
     int _pose_Q_size;
     std::deque<geometry_msgs::PoseWithCovarianceStamped> _pose_Q;
 };
