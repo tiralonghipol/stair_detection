18bbd54c96282a3c81df397287f7a6bac87435ce
tiralonghipol <tiralonghipol@gmail.com>
2020-01-06 17:02:51 -0800

    Revert "added dynamic reconfigure"
    This reverts commit efad194ada0d953b5b4a4d7037845b42e1c33dbe.

--- stairdetect/src/stairdetect_node.cpp
+++ stairdetect/src/stairdetect_node.cpp
@@ -1,22 +1,7 @@
 #include "ros/ros.h"
-#include <dynamic_reconfigure/server.h>
-
 #include "stairdetect/stairdetect.h"
-#include "stairdetect/StairDetectConfig.h"
 
 stairDetectorParams param;
-
-void configCallback(stairdetect::StairDetectConfig &config, uint32_t level)
-{
-    // high level bools
-    param.debug = config.debug;
-    // canny
-    param.canny_low_th = config.canny_low_th;
-    param.canny_ratio = config.canny_ratio;
-    param.canny_kernel_size = config.canny_kernel_size;
-
-    // sd.setParam(param);
-}
 
 int main(int argc, char **argv)
 {
@@ -25,12 +10,7 @@ int main(int argc, char **argv)
 
     stairDetector sd = stairDetector(nh, "stair_detector_node", 100);
     param.debug = true;
-    sd.setParam(param);
-
-    dynamic_reconfigure::Server<stairdetect::StairDetectConfig> dr_srv;
-    dynamic_reconfigure::Server<stairdetect::StairDetectConfig>::CallbackType cb;
-    cb = boost::bind(&configCallback, _1, _2);
-    dr_srv.setCallback(cb);
+    sd.setParam(param);    
 
     ros::spin();
     return 0;
