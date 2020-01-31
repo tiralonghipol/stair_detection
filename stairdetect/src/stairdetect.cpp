bffb8b94144d0da780dfd054c218d1c5c1b6b95a
tiralonghipol <tiralonghipol@gmail.com>
2020-01-30 20:29:51 -0800

    not working yet

--- stairdetect/src/stairdetect.cpp
+++ stairdetect/src/stairdetect.cpp
@@ -168,7 +168,9 @@ void stairDetector::segment_surface(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
 void stairDetector::callback_stitched_pcl(const pcl::PointCloud<pcl::PointXYZ>::Ptr &msg)
 {
     pcl::PointCloud<pcl::PointXYZ>::Ptr subset_cloud = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>), cloud_f(new pcl::PointCloud<pcl::PointXYZ>);
 
     if (!msg->empty())
     {
@@ -204,8 +206,83 @@ void stairDetector::callback_stitched_pcl(const pcl::PointCloud<pcl::PointXYZ>::
         // stair_marker.color.g = 1;
         // stair_marker.color.a = 0.5;
         // _pub_stair_marker.publish(stair_marker);
-        // // _recent_cloud = cloud_filtered;
-        
+
+        _actual_cloud = msg;
+        pcl::VoxelGrid<pcl::PointXYZ> vg;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+        vg.setInputCloud(_actual_cloud);
+        vg.setLeafSize(0.02f, 0.02f, 0.02f);
+        vg.filter(*cloud_filtered);
+        // std::cout << "PointCloud after filtering has: " << cloud_filtered->points.size() << " data points." << std::endl; //*
+        pcl::SACSegmentation<pcl::PointXYZ> seg;
+        pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+        pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>());
+        seg.setOptimizeCoefficients(true);
+        seg.setModelType(pcl::SACMODEL_PLANE);
+        seg.setMethodType(pcl::SAC_RANSAC);
+        seg.setMaxIterations(100);
+        seg.setDistanceThreshold(0.02);
+
+        int i = 0, nr_points = (int)cloud_filtered->points.size();
+        while (cloud_filtered->points.size() > 0.3 * nr_points)
+        {
+            // Segment the largest planar component from the remaining cloud
+            seg.setInputCloud(cloud_filtered);
+            seg.segment(*inliers, *coefficients);
+            if (inliers->indices.size() == 0)
+            {
+                std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+                break;
+            }
+
+            // Extract the planar inliers from the input cloud
+            pcl::ExtractIndices<pcl::PointXYZ> extract;
+            extract.setInputCloud(cloud_filtered);
+            extract.setIndices(inliers);
+            extract.setNegative(false);
+
+            // Get the points associated with the planar surface
+            extract.filter(*cloud_plane);
+            // std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size() << " data points." << std::endl;
+
+            // Remove the planar inliers, extract the rest
+            extract.setNegative(true);
+            extract.filter(*cloud_f);
+            *cloud_filtered = *cloud_f;
+        }
+        // Creating the KdTree object for the search method of the extraction
+        pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+        tree->setInputCloud(cloud_filtered);
+
+        std::vector<pcl::PointIndices> cluster_indices;
+        pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+        ec.setClusterTolerance(0.02); // 2cm
+        ec.setMinClusterSize(100);
+        ec.setMaxClusterSize(25000);
+        ec.setSearchMethod(tree);
+        ec.setInputCloud(cloud_filtered);
+        ec.extract(cluster_indices);
+
+        int j = 0;
+        for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
+        {
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+            for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit)
+                cloud_cluster->points.push_back(cloud_filtered->points[*pit]); //*
+            cloud_cluster->width = cloud_cluster->points.size();
+            cloud_cluster->height = 1;
+            cloud_cluster->is_dense = false;
+
+            // std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size() << " data points." << std::endl;
+            // std::stringstream ss;
+            // ss << "cloud_cluster_" << j << ".pcd";
+            // writer.write<pcl::PointXYZ>(ss.str(), *cloud_cluster, false); //*
+            sensor_msgs::PointCloud2 msg_out;
+            pcl::toROSMsg(*cloud_cluster, msg_out);
+            _pub_stair_plane.publish(msg_out);
+            j++;
+        }
     }
     else
     {
